6b2df88e3cbf867eb7c83905e305620d48655373
harvey254 <harveybill74@gmail.com>
2024-07-16 11:16:58 +0300

    fab

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,43 +1,33 @@
 #!/usr/bin/python3
-"""Clean all archives based on the number of
-arguements passed"""
+"""
+Deletes out-of-date archives
+fab -f 100-clean_web_static.py do_clean:number=2
+    -i ssh-key -u ubuntu > /dev/null 2>&1
+"""
 
-from operator import length_hint
-from fabric.api import run, local, cd, env
 import os
+from fabric.api import *
 
-# env.hosts = ['3.235.198.120', '3.239.50.204']
+env.hosts = ['34.201.165.171', '100.26.240.241']
 
 
 def do_clean(number=0):
-    """Cleans all .tgz files"""
-    """if os.path.exists('versions'):
-        # with cd('versions'):
-        # local('find ')
-        path = 'versions'
-        files = [file for file in os.listdir(
-            path) if 'web' in file and '.tgz' in file]
+    """Delete out-of-date archives.
+    Args:
+        number (int): The number of archives to keep.
+    If number is 0 or 1, keeps only the most recent archive. If
+    number is 2, keeps the most and second-most recent archives,
+    etc.
+    """
+    number = 1 if int(number) == 0 else int(number)
 
-        print(files)
+    archives = sorted(os.listdir("versions"))
+    [archives.pop() for i in range(number)]
+    with lcd("versions"):
+        [local("rm ./{}".format(a)) for a in archives]
 
-        length = len(files)
-        if int(number) > length:
-            exit
-        if int(number) == 0 or int(number) == 1:
-            last = 1
-        else:
-            last = int(number)
-
-        if files:
-            for index in range(length - last):
-              local('rm versions/{}'.format(files[index]))"""
-
-    number = int(number)
-    if number == 0:
-        number = 2
-    else:
-        number += 1
-
-    local('cd versions ; ls -t | tail -n +{} | xargs rm -rf'.format(number))
-    path = '/data/web_static/releases'
-    run('cd {} ; ls -t | tail -n +{} | xargs rm -rf'.format(path, number))
+    with cd("/data/web_static/releases"):
+        archives = run("ls -tr").split()
+        archives = [a for a in archives if "web_static_" in a]
+        [archives.pop() for i in range(number)]
+        [run("rm -rf ./{}".format(a)) for a in archives]
